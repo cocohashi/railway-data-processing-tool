861a9df841fd3c7c2c4873c45449cd304fbd6f1b
Hasier <hasiertxollo@gmail.com>
2024-08-09 13:18:37 +0200

    refactor: redundant comment

--- src/data_plotter.py
+++ src/data_plotter.py
@@ -14,7 +14,6 @@ logger.setLevel(logging.INFO)
 formatter = logging.Formatter(fmt='%(asctime)s - %(name)s - %(levelname)s: %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-# -----------------------------------------------------------------------------------------------------------------
 
 
 class DataPlotter:
