30e7f36820fd47fc59614f47a66f1ba6601abf47
Hasier <hasiertxollo@gmail.com>
2024-08-21 12:16:29 +0200

    refactor: Move config parameters to a config.py file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import logging
-import asyncio
 
 os.environ['ENVIRONMENT'] = "develop"  # 'develop' and 'production' environments only allowed
 
@@ -8,6 +7,7 @@ from src.data_plotter import DataPlotter
 from src.batch_data_generator import BatchDataGenerator
 from src.buffer_manager import BufferManager
 from src.json_file_manager import JsonFileManager
+from src.config import config
 
 # -------------------------------------------------------------------------------------------------------------------
 # Set Logger
@@ -35,66 +35,6 @@ else:
     data_path = "../data/ETS"
     output_path = "./test/output"
     # ----------------------------
-
-# -----------------------------------------------------------------------------------------------------------------
-# Confing Parameters
-# -----------------------------------------------------------------------------------------------------------------
-
-config = {
-
-    # Signal Processor
-    "signal": {
-        "N": 5,  # int: Downsampling-Factor: Number of Samples to be downsampled
-        "f_order": 4,  # int: The order of the Butterworth filter.
-        "Wn": 0.8,  # int or list: Cutoff frequencies of Butterworth filter
-        "btype": "hp",  # str: Butterworth filter type. {‘lowpass’, ‘highpass’, ‘bandpass’, ‘bandstop’}, optional
-        "fs": 1000,  # int: The sampling frequency of the digital system in Hz.
-    },
-
-    # Data Plotting
-    "plot-matrix": {
-        "section": 0,
-        "vmin": None,
-        "vmax": None,
-        "xlabel": "x-axis (samples)",
-        "ylabel": "y-axis (samples)",
-        "title": "",
-        "cmap": "seismic",
-        "figsize": None,
-        "extent": None
-    },
-
-    # Section Map
-    "section-map": {
-        "S01": (95, 200),
-        "S02": (201, 330),
-        "S03": (331, 370),
-        # "S04": (371, 635),
-    },
-
-    # Batch Data Generator
-    "batch-data-generator": {
-        "max-files": 4,
-        "waiting-time": 0.05
-    },
-
-    # Buffer Manager
-    "buffer-manager": {
-        "batch-time": 5,  # Time [s]
-        "spatial-resolution": 5,  # Space [m]
-        "section-train-speed-mean": [144, 144, 144, 144],  # Speed [Km / h]
-        "start-margin-time": 10,  # Time [s]
-        "end-margin-time": 20,  # Time [s]
-    },
-
-    "json-file-manager": {
-        "max-file-size-mb": 3,
-        "save-binary": False
-    }
-}
-
-
-# -----------------------------------------------------------------------------------------------------------------
 
 
 def main():
---
+++ src/config.py
@@ -0,0 +1,58 @@
+# -----------------------------------------------------------------------------------------------------------------
+# Confing Parameters
+# -----------------------------------------------------------------------------------------------------------------
+
+config = {
+
+    # Signal Processor
+    "signal": {
+        "N": 5,  # int: Downsampling-Factor: Number of Samples to be downsampled
+        "f_order": 4,  # int: The order of the Butterworth filter.
+        "Wn": 0.8,  # int or list: Cutoff frequencies of Butterworth filter
+        "btype": "hp",  # str: Butterworth filter type. {‘lowpass’, ‘highpass’, ‘bandpass’, ‘bandstop’}, optional
+        "fs": 1000,  # int: The sampling frequency of the digital system in Hz.
+    },
+
+    # Data Plotting
+    "plot-matrix": {
+        "section": 0,
+        "vmin": None,
+        "vmax": None,
+        "xlabel": "x-axis (samples)",
+        "ylabel": "y-axis (samples)",
+        "title": "",
+        "cmap": "seismic",
+        "figsize": None,
+        "extent": None
+    },
+
+    # Section Map
+    "section-map": {
+        "S01": (95, 200),
+        "S02": (201, 330),
+        "S03": (331, 370),
+    },
+
+    # Batch Data Generator
+    "batch-data-generator": {
+        "max-files": 4,
+        "waiting-time": 0.05
+    },
+
+    # Buffer Manager
+    "buffer-manager": {
+        "batch-time": 5,  # Time [s]
+        "spatial-resolution": 5,  # Space [m]
+        "section-train-speed-mean": [144, 144, 144, 144],  # Speed [Km / h]
+        "start-margin-time": 10,  # Time [s]
+        "end-margin-time": 20,  # Time [s]
+    },
+
+    # JSON File Manager
+    "json-file-manager": {
+        "max-file-size-mb": 3,
+        "save-binary": False
+    }
+}
+
+# -----------------------------------------------------------------------------------------------------------------
