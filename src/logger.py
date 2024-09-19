ee0658eebc24e3a7d256e656bfd8e483aa808d64
Hasier <hasiertxollo@gmail.com>
2024-09-19 15:22:35 +0200

    refactor: logger: Make filelog output selectable and not dependant on production environment

--- src/logger.py
+++ src/logger.py
@@ -2,10 +2,10 @@ import os
 import logging
 
 
-def load_logger(name):
+def load_logger(name, filelog=False):
     logger = logging.getLogger(name)
     logger.propagate = False
-    handler = logging.StreamHandler() if os.environ['ENVIRONMENT'] == 'develop' else logging.FileHandler('main.log')
+    handler = logging.StreamHandler() if not filelog else logging.FileHandler('main.log')
     logger.setLevel(logging.DEBUG) if os.environ['LEVEL'] == 'debug' else logger.setLevel(logging.INFO)
     formatter = logging.Formatter(fmt='%(asctime)s - %(name)s - %(levelname)s: %(message)s',
                                   datefmt="%Y-%m-%d %H:%M:%S")
